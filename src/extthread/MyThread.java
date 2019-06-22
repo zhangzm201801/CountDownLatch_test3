da9a175980f546475703e983cc4531d3152de529
zhangzm201801 <zhangzm1117@126.com>
2019-06-22 17:37:51 +0900

    各就各位准备比赛

---
+++ src/extthread/MyThread.java
@@ -0,0 +1,26 @@
+package extthread;
+
+import java.util.concurrent.CountDownLatch;
+
+public class MyThread extends Thread {
+
+	private CountDownLatch maxRuner;
+
+	public MyThread(CountDownLatch maxRuner) {
+		super();
+		this.maxRuner = maxRuner;
+	}
+
+	@Override
+	public void run() {
+		try {
+			System.out.println(Thread.currentThread().getName());
+			Thread.sleep(2000);
+			maxRuner.countDown();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+}
---
+++ src/test/run/Run.java
@@ -0,0 +1,19 @@
+package test.run;
+
+import java.util.concurrent.CountDownLatch;
+
+import extthread.MyThread;
+
+public class Run {
+	public static void main(String[] args) throws InterruptedException {
+		CountDownLatch maxRuner = new CountDownLatch(10);
+		MyThread[] tArray = new MyThread[10];
+		for (int i = 0; i < tArray.length; i++) {
+			tArray[i] = new MyThread(maxRuner);
+			tArray[i].setName("线程" + (i + 1));
+			tArray[i].start();
+		}
+		maxRuner.await();
+		System.out.println("都回来了！");
+	}
+}
